a5744d893aaad9951aebbfff2fba57b1d5ba85b5
Jakub Werner-local <jakwer-4@student.ltu.se>
2026-03-12 17:27:04 +0100

    Decision change save

--- AIProg4_StrategicAIRemaster/Decisions.h
+++ AIProg4_StrategicAIRemaster/Decisions.h
@@ -36,6 +36,9 @@ namespace CommanderDecisions
 		DecisionTreeNode* makeDecision() override;
 	};
 
+	/// <summary>
+	/// Checks if building is standing
+	/// </summary>
 	class BuildingIsPreplaced : Decision
 	{
 	public:
